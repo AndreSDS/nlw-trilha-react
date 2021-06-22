38666216d1badcc92c53de6298e0377fb5a35262
AndreSDS <andre_smiths@outlook.com>
2021-06-22 14:07:12 -0300

    instalando react-router-dom e configurando navegação

---
+++ letaskme/src/pages/NewRoom/index.tsx
@@ -0,0 +1,34 @@
+import {Link} from 'react-router-dom';
+
+import { Button } from "../../components/Button";
+
+import illustration from '../../assets/images/illustration.svg';
+import logo from '../../assets/images/logo.svg';
+import googleIcon from '../../assets/images/google-icon.svg';
+
+import '../../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+      <div id="page-auth">
+        <aside>
+          <img src={illustration} alt="Ilustração simbolizando perguntas e respostas" />
+          <strong>Toda pergunta tem uma resposta.</strong>
+          <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+        </aside>
+  
+        <main>
+          <div className="main-content">
+            <img src={logo} alt="Logo Letaskme" />
+            <h2>Criar uma nova sala</h2>
+            <form>
+              <input type="text" placeholder="Nome da sala" />
+              <Button type="submit">Criar sala</Button>
+            </form>
+              <p>Quer entrar em uma sala existente?<Link to="/"> Clique aqui</Link>
+              </p>
+          </div>
+        </main>
+      </div>
+  );
+}
